f8ab0e4a87ab2c58a1189af8ec24599aaf9f5fd3
Captain Duane <h3teroerectus@gmail.com>
2020-12-02 13:01:20 -0700

    Added day 2

---
+++ day2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sandw on 12/2/2020.
+//
+
+#include "Day2.h"
---
+++ day2.h
@@ -0,0 +1,14 @@
+//
+// Created by sandw on 12/2/2020.
+//
+
+#ifndef ADVENT2020_DAY2_H
+#define ADVENT2020_DAY2_H
+
+
+class Day2 {
+
+};
+
+
+#endif //ADVENT2020_DAY2_H
